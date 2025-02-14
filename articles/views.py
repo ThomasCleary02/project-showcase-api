e7814b72a62bbc62ba7faa4d9b0b09b296829120
Thomas <thomcleary15@gmail.com>
2025-02-13 20:10:07 -0500

    bug fix

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import Article
 from .serializers import ArticleSerializer
 
-class ProjectViewSet(viewsets.ModelViewSet):
+class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
